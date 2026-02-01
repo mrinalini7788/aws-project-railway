7e025779aefe40e6e76a6b0929167c6abfc69864
Mrinalini R <62600805+mrinalini7788@users.noreply.github.com>
2026-02-02 00:28:08 +0530

    Update outputs.tf

--- aws-terraform-new/security-groups/outputs.tf
+++ aws-terraform-new/security-groups/outputs.tf
@@ -1,18 +1,15 @@
-# security-groups/outputs.tf
-
 output "sg_ec2_sg_ssh_http_id" {
-  # Change 'ec2_sg' to whatever comes after 'resource "aws_security_group"' in your main.tf
-  value = aws_security_group.ec2_sg.id 
-}
-
-output "eks_cluster_sg_id" {
-  value = aws_security_group.eks_cluster.id
-}
-
-output "eks_node_sg_id" {
-  value = aws_security_group.eks_nodes.id
+  value = aws_security_group.ec2_sg_ssh_http.id
 }
 
 output "rds_mysql_sg_id" {
-  value = aws_security_group.rds_sg.id
+  value = aws_security_group.rds_mysql_sg.id
+}
+
+output "eks_cluster_sg_id" {
+  value = aws_security_group.eks_cluster_sg.id
+}
+
+output "eks_node_sg_id" {
+  value = aws_security_group.eks_node_sg.id
 }
