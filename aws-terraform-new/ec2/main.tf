863a598842659d2f50be081c1ba75dc6b2ee398e
Mrinalini R <62600805+mrinalini7788@users.noreply.github.com>
2026-02-01 17:21:27 +0530

    Update main.tf

--- aws-terraform-new/ec2/main.tf
+++ aws-terraform-new/ec2/main.tf
@@ -1,17 +1,14 @@
+# ec2/main.tf
+
 resource "aws_instance" "jump_vm" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.security_group_ids
   key_name                    = var.key_name
-  
-  # Ensures the OS initializes with a public interface
-  associate_public_ip_address = true 
+  associate_public_ip_address = true # MUST be true
 
-  tags = { Name = "JumpVM" }
-}
-
-# Add an output here so the IP goes to the root
-output "public_ip" {
-  value = aws_instance.jump_vm.public_ip
+  tags = {
+    Name = "JumpVM"
+  }
 }
