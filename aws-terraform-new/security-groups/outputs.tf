08f0116e3d1a2dcad63caa31acb7bc0178e12cbe
Mrinalini R <62600805+mrinalini7788@users.noreply.github.com>
2026-02-02 00:23:58 +0530

    Update outputs.tf

--- aws-terraform-new/security-groups/outputs.tf
+++ aws-terraform-new/security-groups/outputs.tf
@@ -1,15 +1,15 @@
 output "sg_ec2_sg_ssh_http_id" {
-  value = aws_security_group.ec2_sg_ssh_http.id
-}
-
-output "rds_mysql_sg_id" {
-  value = aws_security_group.rds_mysql_sg.id
+  value = aws_security_group.ec2_sg.id
 }
 
 output "eks_cluster_sg_id" {
-  value = aws_security_group.eks_cluster_sg.id
+  value = aws_security_group.eks_cluster.id
 }
 
 output "eks_node_sg_id" {
-  value = aws_security_group.eks_node_sg.id
+  value = aws_security_group.eks_nodes.id
+}
+
+output "rds_mysql_sg_id" {
+  value = aws_security_group.rds_sg.id
 }
